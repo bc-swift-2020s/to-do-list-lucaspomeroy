3b5f219b91c76215165cfe145e14b19a072d24c7
Lucas Pomeroy <pomeroyl@bc.edu>
2020-02-10 16:02:01 -0500

    5.2

--- toDoList1/toDoListViewController.swift
+++ toDoList1/toDoListViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class toDoListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["You are Awesome!", "Build Apps!", "Learn Swift.", "Take a vacation!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
+
+extension toDoListViewController: UITableViewDelegate ,UITableViewDataSource{
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return toDoArray.count
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = toDoArray[indexPath.row]
+    return cell
+}
+}
+
 
