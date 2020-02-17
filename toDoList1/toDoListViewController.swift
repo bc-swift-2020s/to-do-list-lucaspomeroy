7b627e4fbe01cc43a2d4db31915120c8c42ea47b
Lucas Pomeroy <pomeroyl@bc.edu>
2020-02-17 15:30:11 -0500

    5.7 done

--- toDoList1/toDoListViewController.swift
+++ toDoList1/toDoListViewController.swift
@@ -21,6 +21,36 @@ class toDoListViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        loadData()
+    }
+    
+    func loadData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos") .appendingPathExtension("json")
+        
+        guard let data = try? Data(contentsOf: documentURL) else {return}
+        let jsonDecoder = JSONDecoder()
+        do {
+            toDoItems = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
+            tableView.reloadData()
+        }catch{
+            print("Error Loading Data \(error.localizedDescription)")
+            
+        }
+    }
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("todos") .appendingPathExtension("json")
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(toDoItems)
+        do{
+            try data?.write(to: documentURL, options: .noFileProtection)
+        }catch{
+            print("Error Saving Data \(error.localizedDescription)")
+        }
+         
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -47,6 +77,7 @@ class toDoListViewController: UIViewController {
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
             
         }
+        saveData()
     }
     
     
@@ -82,13 +113,16 @@ func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> U
             toDoItems.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
+        saveData()
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let itemToMove = toDoItems[sourceIndexPath.row]
         toDoItems.remove(at: sourceIndexPath.row)
         toDoItems.insert(itemToMove, at: destinationIndexPath.row)
+        saveData()
     }
+    
 }
 
 
